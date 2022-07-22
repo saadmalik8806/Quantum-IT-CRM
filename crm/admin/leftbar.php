da9ff949bf8837dc0a6ab0bb9af28f9020189c13
saadmalik8806 <82382491+saadmalik8806@users.noreply.github.com>
2022-07-22 12:31:03 +0530

    hi
    Quantum IT

---
+++ crm/admin/leftbar.php
@@ -0,0 +1,22 @@
+  <div class="page-sidebar" id="main-menu">
+    <div class="page-sidebar-wrapper scrollbar-dynamic" id="main-menu-wrapper">
+      <div class="user-info-wrapper">
+        <div class="profile-wrapper" style="border:solid #fff 1px;"> <img src="../assets/img/admin.png"  alt="" data-src="../assets/img/admin.png" data-src-retina="../assets/img/admin.png" width="69" height="69"  /> </div>
+        <div class="user-info">
+          <div class="greeting">Welcome</div>
+          <div class="username">Admin</div>
+
+        </div>
+      </div>
+
+      <p class="menu-title">BROWSE <span class="pull-right"><a href="javascript:;"><i class="fa fa-refresh"></i></a></span></p>
+
+    <ul>	
+      <li class="start"> <a href="home.php"> <i class="icon-custom-home"></i> <span class="title">Dashboard</span>  </a> 
+		    </li>
+    
+          <li><a href="change-password.php"><span class="fa fa-file-text-o"></span> Change Password</a></li>
+                            <li><a href="manage-users.php"><span class="fa fa-users"></span> Users</a></li>
+                          
+                            <li><a href="user-access-log.php"><span class="fa fa-users"></span>&nbsp;&nbsp;User Access Log</a></li>                            
+                          </ul>
